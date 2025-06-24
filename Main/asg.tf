5dffb069448bb9ddaa03e926badc493633e8e67a
Afonsosequeira04 <afonsosequeira350@gmail.com>
2025-06-24 22:32:48 +0100

    Created two different asg to fix deployment of ec2 in public and private subents

--- Main/asg.tf
+++ Main/asg.tf
@@ -59,21 +59,19 @@ module "asg_sg" {
 # Autoscaling scaling group (ASG)
 ################################################################################
 
-module "asg" {
+# ASG for Public Subnets (with public IPs)
+module "asg_public" {
   source = "terraform-aws-modules/autoscaling/aws"
 
-  # Autoscaling group
-  name = var.asg_name
-
+  name                      = "${var.asg_name}-public"
   min_size                  = var.asg_min_size
   max_size                  = var.asg_max_size
   desired_capacity          = var.asg_desired_capacity
   wait_for_capacity_timeout = var.asg_wait_for_capacity_timeout
   health_check_type         = var.asg_health_check_type
-  vpc_zone_identifier       = concat(module.vpc.public_subnets, module.vpc.private_subnets)
+  vpc_zone_identifier       = module.vpc.public_subnets
   user_data                 = base64encode(local.user_data)
 
-  # Launch template
   launch_template_name        = var.asg_launch_template_name
   launch_template_description = var.asg_launch_template_description
   update_default_version      = var.asg_update_default_version
@@ -83,7 +81,6 @@ module "asg" {
   ebs_optimized     = var.asg_ebs_optimized
   enable_monitoring = var.asg_enable_monitoring
 
-  # IAM role & instance profile
   create_iam_instance_profile = var.asg_create_iam_instance_profile
   iam_role_name               = var.asg_iam_role_name
   iam_role_path               = var.asg_iam_role_path
@@ -95,7 +92,6 @@ module "asg" {
 
   block_device_mappings = [
     {
-      # Root volume
       device_name = "/dev/xvda"
       no_device   = 0
       ebs = {
@@ -104,7 +100,86 @@ module "asg" {
         volume_size           = var.asg_block_device_mappings_volume_size_0
         volume_type           = "gp2"
       }
-      }, {
+    },
+    {
+      device_name = "/dev/sda1"
+      no_device   = 1
+      ebs = {
+        delete_on_termination = true
+        encrypted             = true
+        volume_size           = var.asg_block_device_mappings_volume_size_1
+        volume_type           = "gp2"
+      }
+    }
+  ]
+
+  network_interfaces = [
+    {
+      delete_on_termination       = true
+      description                 = "eth0"
+      device_index                = 0
+      security_groups             = [module.asg_sg.security_group_id]
+      associate_public_ip_address = true
+    },
+  ]
+
+  tag_specifications = [
+    {
+      resource_type = "instance"
+      tags          = var.asg_instance_tags
+    },
+    {
+      resource_type = "volume"
+      tags          = var.asg_volume_tags
+    }
+  ]
+
+  tags = var.asg_tags
+}
+
+# ASG for Private Subnets (without public IPs)
+module "asg_private" {
+  source = "terraform-aws-modules/autoscaling/aws"
+
+  name                      = "${var.asg_name}-private"
+  min_size                  = var.asg_min_size
+  max_size                  = var.asg_max_size
+  desired_capacity          = var.asg_desired_capacity
+  wait_for_capacity_timeout = var.asg_wait_for_capacity_timeout
+  health_check_type         = var.asg_health_check_type
+  vpc_zone_identifier       = module.vpc.private_subnets
+  user_data                 = base64encode(local.user_data)
+
+  launch_template_name        = var.asg_launch_template_name
+  launch_template_description = var.asg_launch_template_description
+  update_default_version      = var.asg_update_default_version
+
+  image_id          = var.asg_image_id
+  instance_type     = var.asg_instance_type
+  ebs_optimized     = var.asg_ebs_optimized
+  enable_monitoring = var.asg_enable_monitoring
+
+  create_iam_instance_profile = var.asg_create_iam_instance_profile
+  iam_role_name               = var.asg_iam_role_name
+  iam_role_path               = var.asg_iam_role_path
+  iam_role_description        = var.asg_iam_role_description
+  iam_role_tags               = var.asg_iam_role_tags
+  iam_role_policies = {
+    AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  }
+
+  block_device_mappings = [
+    {
+      device_name = "/dev/xvda"
+      no_device   = 0
+      ebs = {
+        delete_on_termination = true
+        encrypted             = true
+        volume_size           = var.asg_block_device_mappings_volume_size_0
+        volume_type           = "gp2"
+      }
+    },
+    {
       device_name = "/dev/sda1"
       no_device   = 1
       ebs = {
@@ -122,12 +197,14 @@ module "asg" {
       description           = "eth0"
       device_index          = 0
       security_groups       = [module.asg_sg.security_group_id]
+      # No public IP here, so omit associate_public_ip_address or set false
     },
     {
       delete_on_termination = true
       description           = "eth1"
       device_index          = 1
       security_groups       = [module.asg_sg.security_group_id]
+      # No public IP here either
     }
   ]
 
