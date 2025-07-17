12bbf5afebd10c5e05c0bd91cb057b11c381c3f2
Afonsosequeira04 <afonsosequeira350@gmail.com>
2025-07-17 20:34:08 +0100

    Fixed jenkins acess

--- Main/alb.tf
+++ Main/alb.tf
@@ -20,6 +20,17 @@ resource "aws_security_group_rule" "alb_https_ingress" {
   description       = "Allow HTTPS inbound traffic"
 }
 
+# Add this to allow inbound traffic on port 8080 to ALB security group
+resource "aws_security_group_rule" "alb_http_8080_ingress" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  security_group_id = module.alb_http_sg.security_group_id
+  cidr_blocks       = var.alb_sg_ingress_cidr_blocks
+  description       = "Allow HTTP inbound traffic on port 8080"
+}
+
 module "alb" {
   source          = "terraform-aws-modules/alb/aws"
   version         = "~> 6.0"
@@ -29,19 +40,28 @@ module "alb" {
   security_groups = [module.alb_http_sg.security_group_id]
 
   http_tcp_listeners = [
-  {
-    port        = 80
-    protocol    = "HTTP"
+    {
+      port        = 80
+      protocol    = "HTTP"
 
-    action_type = "redirect"
+      action_type = "redirect"
 
-    redirect = {
-      protocol    = "HTTPS"
-      port        = "443"
-      status_code = "HTTP_301"
+      redirect = {
+        protocol    = "HTTPS"
+        port        = "443"
+        status_code = "HTTP_301"
+      }
+    },
+    {
+      port        = 8080
+      protocol    = "HTTP"
+
+      default_action = {
+        type               = "forward"
+        target_group_index = 1  # Jenkins target group index
+      }
     }
-  }
-]
+  ]
 
   https_listeners = [
     {
@@ -102,9 +122,25 @@ module "alb" {
         protocol            = "HTTP"
         matcher             = "200-399"
       }
+    },
+    {
+      name             = "${var.alb_target_group_name}-jenkins"
+      backend_protocol = "HTTP"
+      backend_port     = 8080
+      target_type      = "instance"
+      health_check = {
+        enabled             = true
+        interval            = 30
+        path                = "/"       # Adjust if you want a different health check for Jenkins
+        port                = "8080"
+        healthy_threshold   = 3
+        unhealthy_threshold = 3
+        timeout             = 6
+        protocol            = "HTTP"
+        matcher             = "200-399"
+      }
     }
   ]
 
   tags = var.alb_tags
 }
-
